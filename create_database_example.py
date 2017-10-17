a6be79138e67b6ac50eec7e597854a001238c15f
mdawson <mitchell.dawson@oriel.ox.ac.uk>
2017-10-17 16:25:40 +0100

    can now add column to existing table

--- create_database_example.py
+++ create_database_example.py
@@ -1,42 +1,43 @@
-import SQLite_fun as SQL
+import pySQLite_fun as SQL
 
-###########################
-### create the database ###
-###########################
+
+#########################
+# create the database ###
+#########################
 
 database = "./dog_example.db"
 
 # create a database connection
 conn = SQL.create_connection(database)
 
-#########################################
-### create the tables of the database ###
-#########################################
+#######################################
+# create the tables of the database ###
+#######################################
 
 create_table_sql = []
 
 create_table_sql.append(""" CREATE TABLE IF NOT EXISTS dogs (
-	id_dog				integer		PRIMARY KEY,
-	dog_name			text		UNIQUE,
-	id_dog_owner		integer,
-	age_of_dog			integer		DEFAULT 0
+    id_dog              integer     PRIMARY KEY,
+    dog_name            text        UNIQUE,
+    id_dog_owner        integer,
+    age_of_dog          integer     DEFAULT 0
 ); """)
 
 create_table_sql.append(""" CREATE TABLE IF NOT EXISTS dog_owners (
-	id_dog_owner	integer     PRIMARY KEY,
-	owner_forename	text,       
-	owner_surname	text
+    id_dog_owner    integer     PRIMARY KEY,
+    owner_forename  text,
+    owner_surname   text
 ); """)
 
-### create database tables ###
+# create database tables ###
 
 if conn is not None:
 
-	for sql in create_table_sql:
+    for sql in create_table_sql:
 
-		SQL.create_table(conn, sql)
+        SQL.create_table(conn, sql)
 
-	print "%s has been created" %database
-		
+    print "%s has been created" % database
+
 else:
-	print("Error! cannot create the database connection.")
+    print("Error! cannot create the database connection.")
--- populate_database_example.py
+++ populate_database_example.py
@@ -1,4 +1,4 @@
-import SQLite_fun as SQL
+import pySQLite_fun as SQL
 
 #######################
 # connect to database #
@@ -7,23 +7,22 @@ database = "./dog_example.db"
 
 conn = SQL.create_connection(database)
 
+########################
+# populate the table ###
+########################
 
+# add some owners to table
+value_tuple = (('John', 'Smith'), ('James', 'Williams'),
+               ('Christopher', 'Jones'))
 
-##########################
-### populate the table ###
-##########################
-
-#add some owners to table
-value_tuple = (('John', 'Smith'), ('James','Williams'), ('Christopher','Jones'))
-
-column_tuple = ('owner_forename','owner_surname')
+column_tuple = ('owner_forename', 'owner_surname')
 
 SQL.insert_many_rows(conn, 'dog_owners', column_tuple, value_tuple)
 
 
-
 # add some dogs to table
-value_tuple = ( ('fido',1,3), ('rover',2,5), ('buddy',2,3), ('lucky',3,2) )
+value_tuple = (('fido', 1, 3), ('rover', 2, 5), ('buddy', 2, 3),
+               ('lucky', 3, 2))
 
 column_tuple = ('dog_name', 'id_dog_owner', 'age_of_dog')
 
@@ -32,45 +31,55 @@ SQL.insert_many_rows(conn, 'dogs', column_tuple, value_tuple)
 conn.commit()
 
 
-
-#################################
-### change the owner of a dog ###
-#################################
+###############################
+# change the owner of a dog ###
+###############################
 
 # Match rover with John Smith
 
 
 # use a SELECT statament to find dog and owner primary key ids
-sql = """SELECT id_dog_owner 
-FROM dog_owners 
-WHERE (owner_forename = 'John' 
+sql = """SELECT id_dog_owner
+FROM dog_owners
+WHERE (owner_forename = 'John'
 AND owner_surname = 'Smith');"""
 
 results = SQL.select(conn, sql)
 
 id_dog_owner = int(results[0][0])
 
-
-
-
-sql = """SELECT id_dog 
-FROM dogs 
+sql = """SELECT id_dog
+FROM dogs
 WHERE dog_name = 'rover';"""
 
 results = SQL.select(conn, sql)
 
 id_dog = int(results[0][0])
 
-
-
-
 # update the row in database
 SQL.update(conn, 'dogs', ('id_dog_owner',), (id_dog_owner,), 'id_dog', id_dog)
 
 conn.commit()
 
+#################################################
+# add another column to database and populate ###
+#################################################
 
-##########################
-### close the database ###
-##########################
+# add owner ages to dog owners table
+
+table_name = 'dog_owners'
+column_name = 'owner_age'
+column_type = 'integer'
+
+# add new column
+SQL.add_column(conn, table_name, column_name, column_type)
+
+# update each dog owners age
+SQL.update(conn, 'dog_owners', ('owner_age',), (20,), 'id_dog_owner', 1)
+SQL.update(conn, 'dog_owners', ('owner_age',), (25,), 'id_dog_owner', 2)
+SQL.update(conn, 'dog_owners', ('owner_age',), (42,), 'id_dog_owner', 3)
+
+conn.commit()
+
+# close the database ###
 conn.close()
